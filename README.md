6fc833f3f705dff9294fe76def8827eecaa08eeb
CEkdhl <c@ekdhl.net>
2016-09-03 01:33:51 +0200

    v0.8

--- README.md
+++ README.md
@@ -1,2 +1,213 @@
-# jSoupLink
-HTML parser for Mathematica/Wolfram Language. More information is available on [Mathematica.Stackexchange](http://mathematica.stackexchange.com/questions/71914/extract-information-from-html-using-css-selectors).
+# jsoupLink
+
+Created by Calle Ekdahl.
+
+GPL-2.0+ licensed.
+
+Current version: 0.8
+
+## Introduction
+
+[jsoup](http://jsoup.org) is an open-source library written in Java, it excels at parsing HTML and manipulating the DOM. jsoupLink is a package written for Mathematica in Wolfram Language which aims to provide an interface to jsoup that feels natural for Mathematica users.
+
+While traditionally HTML has been worked on in Mathematica by importing it as symbolic XML and painstakingly transforming it with pattern matching, jsoupLink introduces the concept of HTML element objects, which make it super easy to traverse the DOM tree and to modify it.
+
+The most common application for jsoupLink is to extract information from websites, for example table data.
+
+## Installing jsoupLink
+Download the files in this repository, put them in a directory named "jsoupLink" and put the directory inside
+
+    FileNameJoin[{$UserBaseDirectory, "Applications"}]
+    
+Use `Needs` to load jsoupLink:
+
+    Needs["jsoupLink`"]
+
+## Importing and Exporting Documents
+It is easy to import and export HTML using jsoupLink, with the built-in `Import` and `Export` commands. Specify `HTMLDOM` as the file format.
+
+![Mathematica graphics](http://i.stack.imgur.com/5yVE6.png)
+
+The returned value is an HTML element object. It has properties that can be used to access information about itself or its children. It also has properties that can modify itself or its children. Having modified the object, exporting it back to HTML is equally simple:
+
+![Mathematica graphics](http://i.stack.imgur.com/VclVR.png)
+
+## HTML Elements
+HTML is but a bunch of nested elements. `<div><p>Paragraph 1</p><p>Paragraph 2</p></div>` is made up of a `div` element and two `p` elements, the `div` being the parent to its two children `p`, and the `p`s being siblings. The idea of jsoup is to assign one object to each element, and to relate the objects to each other through properties. The property `Children` of the object corresponding to `div` would list the two objects corresponding to the `p` elements, the property `Parent` on either of the `p` elements would list the object `div`, and the `Siblings` property of either of the `p` elements would list the other `p` element. Furthermore other properties would retrieve other types of information. The `InnerHTML` property of `div` would return `<p>Paragraph 1</p><p>Paragraph 2</p>` as a string, whereas the `OuterHTML` property of the first `p` would return `<p>Paragraph 1</p>`.
+
+jsoupLink provides direct access to all of these objects and their properties. In a notebook, these objects have a distinctive display:
+
+![Mathematica graphics](http://i.stack.imgur.com/JOSg4.png)
+
+Starting with the object corresponding to the outermost element, `html`, various properties can be used to find all other elements of interest. Properties can be retrieved as subvalues of the objects, as in the image.
+
+In difference to normal Wolfram Language expressions, objects representing elements are mutable, and there are several properties that can modify elements. Most properties can be accessed as `obj["property"]`, some take several arguments, e.g. `obj["Attribute", "attributeName"]`, or `obj["Attribute", "key", "value"]`, which will set the attribute `key` to the value `value`.
+
+## Properties
+Throughout this list, objects representing HTML elements will be referred to simply as elements. Elements are arranged in a tree structure, called the DOM tree. Whenever descriptions such as "the same level" or "topmost", or "beneath" are used in the following text, it refers to this tree structure. (See also the first paragraph of the preceding section.)
+
+This is a complete listing of all the properties, available to all elements:
+
+ - `element["TagName"]`
+ Tag name.
+Example: link elements return `a`, paragraph elements return `p`.
+
+ - `element["TagName", "tag"]`
+ Set element tag name.
+ Example: Use to convert  an`h1` element into an `h2` element.
+
+ - `element["Root"]`
+ Topmost element, usually `html`.
+
+ - `element["Parent"]`
+ Immediate ancestor of `element`. 
+ Example: the parent to `body` is `html`.
+
+ - `element["Children"]`
+ All elements that lie directly under `element`.
+ Example: `li` elements are usually children of a `ul`.
+
+ - `element["Siblings"]`
+ All elements on the same level as `element`.
+ Example: The siblings of an `<li>` elements are usually other `<li>` elements.
+
+ - `element["Select", "selector"]`
+All elements from anywhere beneath `element`, that match the CSS selector "selector". More information about valid syntax: [Use selector syntax to find elements](https://jsoup.org/cookbook/extracting-data/selector-syntax).
+
+ - `element["AllElements"]`
+ All elements beneath `element`.
+
+ - `element["InnerHTML"]`
+ HTML corresponding to the offspring of `element`.
+ Example: the inner HTML of `<div><b>Great!</b></div>` is `<b>Great</b>`.
+
+ - `element["OuterHTML"]`
+ HTML corresponding to `element` and all offspring.
+ Example: the outer HTML of `<div><b>Great!</b></div>` is `<div><b>Great!</b></div>`.
+
+ - `element["OwnText"]`
+ Text which resides directly under `element`.
+ Example: the `OwnText` of `<p>text <b>more text</b></p>` is `text`. The `OwnText` of the `b` element is `more text`.
+
+ - `element["AllText"]`
+ All text beneath `element`.
+ Example: `AllText` of the `html` element returns all text in the document.
+
+ - `element["AllText", "text"]`
+ Remove existing elements and text beneath `element` and replace with `"text"`.
+
+ - `element["ID"]`
+The `ID` attribute.
+
+ - `element["ClassNames"]`
+List of classes in the class attribute.
+
+ - `element["Value"]`
+ The `value` attribute, if the element has it.
+
+ - `element["HasAttribute", "attr"]`
+ `True` if the attribute `attr` is given, and `False` otherwise.
+
+ - `element["Attribute", "attr"]`
+Value of the attribute `attr`.
+
+ - `element["Attribute", "attr", "val"]`
+ Set attribute `attr` to the value `val`.
+
+ - `element["Attribute", "attr", True | False]`
+Set attribute `attr` to `""` if `True`, remove `attr` if `False`.
+
+ - `element["Attribute", "assoc"]`
+ Set all attributes as given by the association `assoc`.
+
+ - `element["Attributes"]`
+ Association with all attributes and their values.
+
+ - `element["RemoveAttribute", "attr"]`
+ Remove the attribute `attr`.
+
+ - `element["IsBlock"]`
+ `True` if `element` is a block level element, `False` otherwise.
+
+ - `element["HasText"]`
+ `True` if `element["AllText"]` is not equal to `""`, `False` if it is.
+
+ - `element["BaseURI"]`
+ The base URI of the document.
+
+ - `element["BaseURI", "uri"]`
+Set the base URI of the document.
+
+ - `element["HasClass", "class"]`
+ `True` if `class` appears in `element`'s class attribute, `False` otherwise.
+
+ - `element["AddClass", "class"]`
+Add `class` to `element`'s class attribute.
+
+ - `element["RemoveClass", "class"]`
+ Remove `class` from `element`'s class attribute.
+
+ - `element["ToggleClass", "class"]`
+Add `class` to `element`'s class attribute if it doesn't have it, and remove it if it is already there.
+
+ - `element["Before", "html"]`
+ Parse `html` and insert the resulting object before `element`.
+
+ - `element["Before", el]`
+Insert element `el` before `element`.
+
+ - `element["After", "html"]`
+ Parse `html` and insert the resulting object after `element`.
+
+ - `element["After", el]`
+Insert element `el` after `element`.
+
+ - `element["Prepend", "html"]`
+ Parse `html` and prepend the resulting object to `element`'s children.
+
+ - `element["Prepend", el]`
+Prepend element `el` to `element`'s children.
+
+ - `element["Append", "html"]`
+ Parse `html` and append the resulting object to `element`'s children.
+
+ - `element["Append", el]`
+Append element `el` to `element`'s children.
+
+ - `element["ReplaceWith", el]`
+Replace `element` with element `el`.
+
+ - `element["Remove"]`
+ Remove `element`.
+
+ - `element["Wrap", "html"]`
+ Make `element` a child of the object resulting from parsing `html`.
+
+ - `element["Unwrap"]`
+ Remove `element` but keep its children, essentially moving them up one level.
+
+ - `element["Clean"]`
+ Run `element` and all its offspring through a whitelist. Used to e.g. prevent XSS attacks.
+
+ - `element["DeepCopy"]`
+Return a copy of `element`, such that modifications done to the copy do not affect `element`.
+
+ - `element["Properties"]`
+ List all properties.
+
+ - `element["DOMTree"]`
+ Display the DOM tree. `assoc` can be used to provide options, detailed below.
+
+ - `element["DOMTreeWindow"]`
+ Display the DOM tree in a separate window. `assoc` can be used to provide options, detailed below.
+
+## DOM Tree Interface
+`element["DOMTree"]` opens an interface to view the DOM tree with `element` as root:
+
+![Mathematica Graphics](https://dl.dropboxusercontent.com/u/2736911/domview.gif)
+
+Elements can be selected by clicking on them. The "copy node" button writes the corresponding element to the clipboard, so that it can be pasted into a notebook. "Copy CSS selector" writes a CSS selector that uniquely identifies the selected element to the clipboard.
+
+`element["DOMTree", options]` accepts the following options: `Width`, `Height`, `FontSize`. These options determine how large the display is initially.
+
+`element["DOMTreeWindow", options]` takes the additional options `WindowWidth` and `WindowHeight` to determine the initial size of the window. Appropriate values vary from system to system.
--- jSoupLink.m
+++ jSoupLink.m
@@ -1,76 +1,306 @@
-BeginPackage["jSoupLink`"]
+(* Mathematica Package                     *)
 
-ParseHTML::usage =
-    "ParseHTML[address, selector, keys, useAbsoluteURL] retrieves \
-attributes and content
-  specified by keys and the CSS selector \"selector\" from the URL or \
-local file path address.
-  The last parameter useAbsoluteURL specifies whether retrieved src \
-and href attributes should
-  be absolute or relative. It is optional and defaults to True.";
+(* :Title: jsoupLink                       *)
+(* :Context: jsoupLink`                    *)
+(* :Author: Calle Ekdahl                   *)
+(* :Date: 2016-09-02                       *)
 
-ParseHTMLString::usage =
-    "ParseHTMLString[string, selector, keys] performs the same type of \
-retrieval as ParseHTML, but takes an HTML string instead of a file.";
+(* :Package Version: 0.8                   *)
+(* :Mathematica Version: 11.0              *)
+(* :Copyright: (c) 2016 Calle Ekdahl       *)
+(* :Keywords:                              *)
+(* :Discussion:                            *)
 
-ParseHTMLFragment::usage =
-    "ParseHTMLFragment[string, selector, keys] is just like \
-ParseHTMLString, but if the string isn't a complete HTML document but \
-instead only a part of the body of the document then this method may \
-perform better, since it will add html and body tags to make the \
-document complete before it attempts to parse it.";
+BeginPackage["jsoupLink`"]
+(* Exported symbols added here with SymbolName::usage *)
 
-Begin["`Private`"]
+Begin["`Private`"] (* Begin Private Context *)
+
 Needs["JLink`"]
 InstallJava[];
-LoadJavaClass["org.jsoup.Jsoup"]
+LoadJavaClass["org.jsoup.Jsoup"];
 
-ParseHTML[address_, selector_, keys_] := Module[{data, doc},
-  JavaBlock[
-  data = Import[address, "Text"];
-  If[data == $Failed, Return[$Failed]];
-  doc = Jsoup`parse[data, address];
-  parse[doc, selector, keys]
+ImportExport`RegisterImport["HTMLDOM", jsoupLink`DownloadDOM];
+ImportExport`RegisterExport["HTMLDOM", jsoupLink`ExportDOM];
+
+DownloadDOM[filename_String, opts___] := ParseDOM[Import[filename, "Text", opts]]
+DownloadDOM[$Failed, _String] := $Failed
+
+ParseDOM[html_String, baseUri_String:""] := Module[{doc, root},
+  doc = JavaBlock[Jsoup`parse[html, baseUri]];
+  root = First[doc@children[]@toArray[]];
+  Global`HTMLElement[root]
+]
+
+ExportDOM[filename_, data_, opts___] := Export[filename, data["OuterHTML"], "Text",opts]
+
+icon = Import["assets/documenticon.png"];
+
+MakeBoxes[obj_Global`HTMLElement, fmt_] ^:= Module[{el = First[obj], shown, hidden, icon = Show[icon, ImageSize -> 70]},
+      shown = {
+        {BoxForm`MakeSummaryItem[{"Tag: ", el@tagName[]}, fmt], SpanFromLeft},
+        {BoxForm`MakeSummaryItem[{"Children: ", Length@el@children[]@toArray[]}, fmt], SpanFromLeft},
+        {BoxForm`MakeSummaryItem[{"Has text: ", el@hasText[]}, fmt], SpanFromLeft}
+      };
+      hidden = {
+        {BoxForm`MakeSummaryItem[{"ID: ", el@id[]}, fmt], SpanFromLeft},
+        {BoxForm`MakeSummaryItem[{"Classes: ", Row[el@classNames[]@toArray[], ", "]}, fmt], SpanFromLeft},
+        {BoxForm`MakeSummaryItem[{"Block level: ", el@isBlock[]}, fmt], SpanFromLeft}
+      };
+      BoxForm`ArrangeSummaryBox[Global`HTMLElement, obj, icon, shown, hidden, fmt, "Interpretable" -> True]
+    ];
+
+Global`HTMLElement[el_]["TagName"] := el@tagName[]
+Global`HTMLElement[el_]["TagName", tag_] := el@tagName[tag]
+Global`HTMLElement[el_]["Root"] := Global`HTMLElement[First[el@ownerDocument[]@children[]@toArray[]]]
+Global`HTMLElement[el_]["Parent"] := If[el@tagName[] != "html", Global`HTMLElement[el@parent[]], Global`HTMLElement[el]]
+Global`HTMLElement[el_]["Children"] := Global`HTMLElement /@ el@children[]@toArray[]
+Global`HTMLElement[el_]["Siblings"] := Global`HTMLElement /@ el@siblingElements[]@toArray[]
+Global`HTMLElement[el_]["Select", selector_String] := Global`HTMLElement /@ el@select[selector]@toArray[]
+Global`HTMLElement[el_]["AllElements"] := Global`HTMLElement /@ el@getAllElements[]@toArray[]
+Global`HTMLElement[el_]["Value"] := el@val[]
+Global`HTMLElement[el_]["InnerHTML"] := el@html[]
+Global`HTMLElement[el_]["InnerHTML", newHTML_String] := Global`HTMLElement[el@html[newHTML]]
+Global`HTMLElement[el_]["OuterHTML"] := el@outerHtml[]
+Global`HTMLElement[el_]["OwnText"] := el@ownText[]
+Global`HTMLElement[el_]["AllText"] := el@text[]
+Global`HTMLElement[el_]["AllText", txt_] := Global`HTMLElement[el@text[txt]]
+Global`HTMLElement[el_]["ID"] := el@id[]
+Global`HTMLElement[el_]["ClassNames"] := el@classNames[]@toArray[]
+Global`HTMLElement[el_]["HasAttribute", attribute_String] := el@hasAttr[attribute]
+Global`HTMLElement[el_]["Attribute", attribute_String] := el@attr[attribute]
+Global`HTMLElement[el_]["Attribute", key_String, value_String] := Global`HTMLElement[el@attr[key, value]]
+Global`HTMLElement[el_]["Attribute", assoc_Association] := (KeyValueMap[el@attr[#,#2] &, assoc]; Global`HTMLElement[el])
+Global`HTMLElement[el_]["Attributes"] := Association @@ (#@getKey[] -> #@getValue[] & /@ el@attributes[]@asList[]@toArray[])
+Global`HTMLElement[el_]["RemoveAttribute", attribute_String] := Global`HTMLElement[el@removeAttr[attribute]]
+Global`HTMLElement[el_]["IsBlock"] := el@isBlock[]
+Global`HTMLElement[el_]["HasText"] := el@hasText[]
+Global`HTMLElement[el_]["BaseURI"] := el@baseUri[]
+Global`HTMLElement[el_]["BaseURI", uri_String] := el@setBaseUri[uri]
+Global`HTMLElement[el_]["HasClass", class_String] := el@hasClass[class]
+Global`HTMLElement[el_]["AddClass", class_String] := Global`HTMLElement[el@addClass[class]]
+Global`HTMLElement[el_]["RemoveClass", class_String] := Global`HTMLElement[el@removeClass[class]]
+Global`HTMLElement[el_]["ToggleClass", class_String] := Global`HTMLElement[el@toggleClass[class]]
+Global`HTMLElement[el_]["After", html_String] := Global`HTMLElement[el@after[html]]
+Global`HTMLElement[el_]["Before", html_String] := Global`HTMLElement[el@before[html]]
+Global`HTMLElement[el_]["After", Global`HTMLElement[child_]] := Global`HTMLElement[el@after[child]]
+Global`HTMLElement[el_]["Before", Global`HTMLElement[child_]] := Global`HTMLElement[el@before[child]]
+Global`HTMLElement[el_]["Append", html_String] := Global`HTMLElement[el@append[html]]
+Global`HTMLElement[el_]["Prepend", html_String] := Global`HTMLElement[el@prepend[html]]
+Global`HTMLElement[el_]["Append", Global`HTMLElement[child_]] := Global`HTMLElement[el@appendChild[child]]
+Global`HTMLElement[el_]["Prepend", Global`HTMLElement[child_]] := Global`HTMLElement[el@prependChild[child]]
+Global`HTMLElement[el_]["ReplaceWith", Global`HTMLElement[replacement_]] := Global`HTMLElement[el@replaceWith[replacement]]
+Global`HTMLElement[el_]["Remove"] := (el@remove[]; Null)
+Global`HTMLElement[el_]["Wrap", html_] := Global`HTMLElement[el@wrap[html]]
+Global`HTMLElement[el_]["Unwrap", html_] := With[{p=el@parent[]}, el@unwrap[]; Global`HTMLElement[p]]
+Global`HTMLElement[el_]["Clean"] := Global`HTMLElement[el@html[Jsoup`clean[el@html[]]]]
+Global`HTMLElement[el_]["DeepCopy"] := Global`HTMLElement[JavaBlock[el@clone[]]]
+Global`HTMLElement[el_]["DOMTree"] := tree[el]
+Global`HTMLElement[el_]["DOMTreeWindow"] := popup[el]
+
+properties = {"TagName", "Root", "Parent", "Children", "Siblings",
+  "Select", "AllElements", "Value", "InnerHTML", "OuterHTML",
+  "OwnText", "AllText", "ID", "ClassNames", "HasAttribute",
+  "Attribute", "Attributes", "RemoveAttribute", "IsBlock", "HasText",
+  "BaseURI", "HasClass", "AddClass", "RemoveClass", "ToggleClass",
+  "After", "Before", "Append", "Prepend", "ReplaceWith", "Remove",
+  "Wrap", "Unwrap", "Clean", "DeepCopy", "DOMTree", "DOMTreeWindow"};
+
+Global`HTMLElement[el_]["Properties"] := properties;
+
+hasPropertyQ[prop_] := MemberQ[properties, prop]
+
+HTMLElement::argx = "Property `1` called with the wrong number of arguments."
+HTMLElement::noproperty = "The property `1` does not exist."
+
+Global`HTMLElement[el_][prop_?hasPropertyQ, ___] := (Message[HTMLElement::argx, prop]; $Failed)
+Global`HTMLElement[el_][prop_, ___] := (Message[HTMLElement::noproperty, prop]; $Failed)
+
+(* http://mathematica.stackexchange.com/questions/59768/how-to-call-a-java-method-that-takes-a-boolean-not-boolean
+This may not work on all systems. *)
+Global`HTMLElement[el_]["Attribute", key_String, value:(True | False)] := Global`HTMLElement[el@attr[key, value]]
+
+ElementProperty[node_Global`HTMLElement, property__] := node[property]
+ElementProperty[node_Global`HTMLElement][property__] := node[property]
+
+(* DOM Tree *)
+colors = <|
+    "background" -> RGBColor[{1, 1, 1}],
+    "tag" -> RGBColor[{117, 5, 126}/255 // N],
+    "attributes" -> RGBColor[{137, 73, 0}/255 // N],
+    "arguments" -> RGBColor[{46, 42, 171}/255 // N],
+    "glue" -> RGBColor[{163, 148, 165}/255 // N],
+    "doctype" -> RGBColor[{192, 192, 192}/255 // N],
+    "comment" -> RGBColor[{68, 111, 44}/255 // N],
+    "url" -> RGBColor[{47, 82, 203}/255 // N],
+    "text" -> RGBColor[{54, 60, 68}/255 // N],
+    "highlight" -> RGBColor[{236, 241, 252}/255 // N],
+    "selected" -> <|
+        "background" -> RGBColor[{79, 118, 216}/255 // N],
+        "tag" -> RGBColor[{1, 1, 1}],
+        "attributes" -> RGBColor[{200, 201, 203}/255 // N],
+        "arguments" -> RGBColor[{1, 1, 1}],
+        "glue" -> RGBColor[{157, 160, 167}/255 // N],
+        "doctype" -> RGBColor[{1, 1, 1}],
+        "comment" -> RGBColor[{1, 1, 1}],
+        "url" -> RGBColor[{1, 1, 1}],
+        "text" -> RGBColor[{1, 1, 1}]
+        |>
+    |>;
+
+c::invalidColor = "The color specification `1` is not recognized.";
+Options[c] = {"Selected" -> False};
+c[s_String, type_String, OptionsPattern[]] := If[
+  StringMatchQ[type, "tag" | "attributes" | "arguments" | "glue" | "doctype" | "comment" | "url" | "text"],
+  If[OptionValue["Selected"],
+    ToString[Style[s, colors["selected", type]], StandardForm],
+    ToString[Style[s, colors[type]], StandardForm]
+  ],
+  Message[c::invalidColor, type]; ""
+]
+cs[s_String, type_String] := c[s, type, "Selected" -> True]
+
+c[attributes_Association, c_: c] := StringJoin[KeyValueMap[StringJoin[
+      " ",
+      c[#, "attributes"],
+      c["=\"", "glue"],
+      If[# == "href" || # == "src",
+        ToString[MouseAppearance[Hyperlink[c[#2, "url"], #2, ActiveStyle -> Underlined], "LinkHand"], StandardForm],
+        c[#2, "arguments"]
+      ],
+      c["\"", "glue"]
+    ] &, attributes]]
+cs[attributes_Association] := c[attributes, cs]
+
+elementDescription[el_, c_: c] /; InstanceOf[el, "org.jsoup.nodes.Element"] := If[
+  el@childNodeSize[] > 0,
+  StringJoin[
+    c["<", "glue"],
+    c[el@tagName[], "tag"],
+    c[elementAttributes[el]],
+    c[">\[Ellipsis]</", "glue"],
+    c[el@tagName[], "tag"],
+    c[">", "glue"]
+  ],
+  StringJoin[
+    c["<", "glue"],
+    c[el@tagName[], "tag"],
+    c[elementAttributes[el]],
+    c["/>", "glue"]
   ]
 ]
 
-ParseHTMLString[string_, selector_, keys_] := Module[{doc},
-  JavaBlock[
-  doc = Jsoup`parse@string;
-  parse[doc, selector, keys, False]
-  ]
+elementDescription[el_, ___] /; InstanceOf[el, "org.jsoup.nodes.TextNode"] := If[StringMatchQ[el@text[], Whitespace], Nothing, el@text[]]
+elementDescription[el_, ___] /; InstanceOf[el, "org.jsoup.nodes.DataNode"] := el@getWholeData[]
+
+elementOpen[el_, c_: c] := StringJoin[
+  c["<", "glue"],
+  c[el@tagName[], "tag"],
+  c[elementAttributes[el]],
+  c[">", "glue"]
 ]
 
-ParseHTMLFragment[string_, selector_, keys_] := Module[{doc},
-  JavaBlock[
-  doc = Jsoup`parseBodyFragment[string];
-  parse[doc, selector, keys, False]
-  ]
+elementClose[el_, c_: c] := StringJoin[
+  c["</", "glue"],
+  c[el@tagName[], "tag"],
+  c[">", "glue"]
 ]
 
-parse[doc_, selector_, keys_, useAbsoluteURL_: True] :=
-    Module[{data, elements, res, newKeys},
-      JavaBlock[
-      elements = doc@select@selector;
-      elements = Table[elements@get[i - 1], {i, elements@size[]}];
+elementAttributes[el_] := Module[{attrs},
+  attrs = <|#@getKey[] -> #@getValue[] & /@ el@attributes[]@asList[]@toArray[]|>;
+  If[KeyExistsQ[attrs, "href"] && el@baseUri[] != "", attrs["href"] = el@attr["abs:href"]];
+  If[KeyExistsQ[attrs, "src"] && el@baseUri[] != "", attrs["src"] = el@attr["abs:src"]];
+  attrs
+]
 
-      res = Outer[Switch[#2,
-        "html", #@html[],
-        "ownText", #@ownText[],
-        "text", #@text[],
-        "tagName", #@tagName[],
-        "src", If[useAbsoluteURL, #@absUrl@"src", #@attr@"src"],
-        "href", If[useAbsoluteURL, #@absUrl@"href", #@attr@"href"],
-        _, #@attr@#2
-      ] &,
-        elements,
-        If[Head@keys === String, {keys}, keys]
-      ];
+elementChildren[el_] := el@childNodes[]@toArray[]
 
-      If[Head@keys === String, First /@ res, res]
-      ]
+attachEventHandler[element_, root_, ID_] := Item[
+  EventHandler[element, {
+    {"MouseClicked", 1} :> If[
+      root["selected"] === ID,
+      root["selected"] = Null,
+      root["selected"] = ID
     ]
+  }, PassEventsDown -> True],
+  Alignment -> Left,
+  If[root["selected"] === ID,
+    Background -> colors["selected", "background"],
+    ## &[]
+  ]
+] /; InstanceOf[ID, "org.jsoup.nodes.Element"]
 
-End[]
+attachEventHandler[el_, root_, ID_] /; InstanceOf[ID, "org.jsoup.nodes.TextNode"] := Item[el, Alignment -> Left]
+attachEventHandler[Nothing[], root_, ID_] /; InstanceOf[ID, "org.jsoup.nodes.TextNode"] := Nothing
+attachEventHandler[el_, root_, ID_] /; InstanceOf[ID, "org.jsoup.nodes.DataNode"] := Item[el, Alignment -> Left]
+
+SetAttributes[CustomPaneSelector, HoldRest]
+CustomPaneSelector[x_, true_, false_] := If[x, true, false]
+
+SetAttributes[CustomOpenerView, HoldFirst]
+CustomOpenerView[{heading_, content_, prolog_, epilog_}, root_, ID_, state_] := DynamicModule[{x = state},
+  Dynamic@CustomPaneSelector[x,
+    Grid[{
+      {Opener[Dynamic[x, (root[ID] = x = #) &]], attachEventHandler[prolog, root, ID]},
+      {"", Item[content, Alignment -> Left]},
+      {"", attachEventHandler[epilog, root, ID]}
+    }],
+    Grid[{
+      {Opener[Dynamic[x, (root[ID] = x = #) &]], attachEventHandler[heading, root, ID]}
+    }]
+  ]
+]
+
+renderElement[el_, root_, ID_, state_] := If[
+  Length@elementChildren[el] > 0,
+  CustomOpenerView[{
+    If[root["selected"] === ID, elementDescription[el, cs], elementDescription[el]],
+    Column[renderElement[#, root, #, root@#] & /@ el@childNodes[]@toArray[]],
+    If[root["selected"] === ID, elementOpen[el, cs], elementOpen[el]],
+    If[root["selected"] === ID, elementClose[el, cs], elementClose[el]]
+  }, root, ID, state],
+  attachEventHandler[If[root["selected"] === ID, elementDescription[el, cs], elementDescription[el]], root, ID]
+]
+
+Options[tree] = {
+  "Width" -> 1400,
+  "Height" -> 480,
+  "FontSize" -> 12
+}
+tree[el_, OptionsPattern[]] := DynamicModule[{root},
+  root[_] := False;
+  root["selected"] := Null;
+  Panel[Column[{
+    Dynamic@Pane[Row[{
+      Button["Copy node", CopyToClipboard[Global`HTMLElement[root["selected"]]], Enabled -> (root["selected"] =!= Null)],
+      Button["Copy CSS selector", CopyToClipboard[root["selected"]@cssSelector[]], Enabled -> (root["selected"] =!= Null)]
+    }]],
+    Pane[Deploy@renderElement[el, root, el, False],
+      Scrollbars -> True,
+      BaseStyle -> {Background -> White, FontSize -> OptionValue["FontSize"]},
+      ImageSize -> {OptionValue["Width"], OptionValue["Height"]},
+      FrameMargins -> 20
+    ]
+  }]]
+]
+
+Options[popup] = {
+  "WindowWidth" -> 1420,
+  "WindowHeight" -> 560
+  "Width" -> 1400,
+  "Height" -> 480,
+  "FontSize" -> 12
+}
+popup[el_, opts: OptionsPattern[]] := CreateDocument[tree[el, opts],
+  "CellInsertionPointCell" -> Cell[],
+  ShowCellBracket -> False,
+  WindowElements -> {},
+  WindowFrame -> "Generic",
+  WindowSize -> {OptionValue["WindowWidth"], OptionValue["WindowHeight"]},
+  WindowTitle -> None,
+  WindowToolbars -> {}
+]
+
+End[] (* End Private Context *)
 
 EndPackage[]
